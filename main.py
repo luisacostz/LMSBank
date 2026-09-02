cda53887cc57375717df4084c5360cab9e6f4679
Murilo <murilo.desantana@dcomp.ufs.br>
2026-09-02 20:55:24 -0300

    Implementa integração inicial do sistema

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # Arquivo principal do LMS BANK
+from cliente import cadastrar_cliente
+from conta import consultar_saldo, depositar, sacar
+
+nome = input("Digite seu nome:")
+cpf = input("Digite seu cpf:")
+
+nome, cpf = cadastrar_cliente(nome, cpf)
+saldo = 0
+
+consultar_saldo(saldo)
+
+valor_deposito = float(input("Digite o valor do depósito:"))
+
+
+saldo = depositar(saldo,valor_deposito)
+
+consultar_saldo(saldo)
+
+valor_saque = float(input("Digite o valor do saque:"))
+
+saldo = sacar(saldo, valor_saque)
+
+consultar_saldo(saldo)
