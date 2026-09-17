800bc74b9174ca1d1f6b844bbae9bc1bcf60a5cc
SarahBeatriz0 <sarah.dossantos@dcomp.ufs.br>
2026-09-17 16:47:08 -0300

    Inclusão de agencias

--- armazenamento.py
+++ armazenamento.py
@@ -1,7 +1,7 @@
 import json
 
-def salvar_dados(clientes, contas):
-    dados_completos = [clientes, contas]
+def salvar_dados(clientes, contas, agencias):
+    dados_completos = [clientes, contas, agencias]
     arquivo = open("banco.json", "w")
     json.dump(dados_completos, arquivo)
     arquivo.close()
