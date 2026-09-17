34b466bd1bb8f017eb01e2486ffcdf8c6b0563b3
SarahBeatriz0 <sarah.dossantos@dcomp.ufs.br>
2026-09-17 15:15:15 -0300

    Adicionando função de listar clientes

--- cliente.py
+++ cliente.py
@@ -2,6 +2,15 @@ def cadastrar_cliente(clientes, nome_digitado, cpf_digitado):
     clientes.append((nome_digitado, cpf_digitado))
 
 
+def listar_clientes(clientes):
+    if len(clientes) == 0:
+        print("Nenhum cliente cadastrado.")
+    else:
+        print("\n--- LISTA DE CLIENTES ---")
+        for cliente in clientes:
+            print("Nome:", cliente[0], "- CPF:", cliente[1])
+            
+
 def procurar_cliente(clientes, cpf_digitado):
     for cliente in clientes:
         if cliente[1] == cpf_digitado:
