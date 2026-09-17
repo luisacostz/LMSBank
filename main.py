2cb83796653a905badccfbdfaf2641153777955a
SarahBeatriz0 <sarah.dossantos@dcomp.ufs.br>
2026-09-17 16:40:34 -0300

    Validação de agências e inclusão no salvamento de dados

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cliente import cadastrar_cliente, procurar_cliente
 from conta import cadastrar_conta, listar_contas, consultar_saldo, depositar, sacar, transferir
+from agencia import cadastrar_agencia, listar_agencias, procurar_agencia
 from menu import menu
 from armazenamento import salvar_dados
 
@@ -8,7 +9,7 @@ def main():
     contas = []
     agencias = []
 
-    for funcionamento in range(100):
+    while rodando:
         menu()
         opcao = input("Escolha uma opcao: ")
 
@@ -18,6 +19,9 @@ def main():
             cadastrar_cliente(clientes, nome, cpf)
             salvar_dados(clientes, contas)
             print("Cliente cadastrado com sucesso!")
+
+        elif opcao == "2":
+            listar_clientes(clientes)
 
         elif opcao == "3":
             cpf_busca = input("Digite o CPF do cliente que deseja procurar: ")
@@ -29,11 +33,31 @@ def main():
             else:
                 print("Cliente nao cadastrado no sistema.")
 
+        elif opcao == "4":
+            numero_ag = input("Digite o número da agência: ")
+            nome_ag = input("Digite o nome da agência: ")
+            cadastrar_agencia(agencias, numero_ag, nome_ag)
+            salvar_dados(clientes, contas, agencias)
+
+        elif opcao == "5":
+            listar_agencias(agencias)
+
         elif opcao == "7":
             numero_conta = input("Digite o numero da conta: ")
             numero_agencia = input("Digite o numero da agencia: ")
-            quantidade_titulares = int(input("Quantos titulares a conta tera? "))
-            cpfs_temporarios = []
+            agenciai_valida = procurar_agencia(agencias, numero_agencia)
+            if agencia_valida == None:
+                print('Agência não encontrada! Cadastre a agência.')
+            else:
+                quantidade_titulares = int(input("Quantos titulares a conta tera? "))
+                cpfs_temporarios = []
+                for i in range(quantidade_titulares):
+                    cpf = input("Digite o CPF do titular: ")
+                    clientte_encontrado = procurar_cliente(clientes, cpf)
+                    if cliente_encontrado != None:
+                        cpfs_temporarios.append(cpf):
+                    else:
+                        print("Cliente não encontrado no sistema!")
 
             for i in range(quantidade_titulares):
                 cpf = input("Digite o CPF do titular: ")
@@ -63,25 +87,27 @@ def main():
             numero_conta = input("Digite o numero da conta: ")
             valor = float(input("Digite o valor do deposito: "))
             depositar(contas, numero_conta, valor)
-            salvar_dados(clientes, contas)
+            salvar_dados(clientes, contas, agencias)
 
         elif opcao == "11":
             numero_conta = input("Digite o numero da conta: ")
             valor = float(input("Digite o valor do saque: "))
             sacar(contas, numero_conta, valor)
-            salvar_dados(clientes, contas)
+            salvar_dados(clientes, contas, agencias)
 
         elif opcao == "12":
             numero_origem = input("Digite a sua conta (origem): ")
             numero_destino = input("Digite a conta destino: ")
             valor = float(input("Digite o valor da transferencia: "))
             transferir(contas, numero_origem, numero_destino, valor)
-            salvar_dados(clientes, contas)
+            salvar_dados(clientes, contas, agencias)
 
         elif opcao == "0":
             print("\nObrigado por utilizar o LMS Bank!")
+            rodando = False
 
         else:
             print("\nOpcao invalida ou modulo em desenvolvimento! Tente novamente.")
+            
 
 main()
